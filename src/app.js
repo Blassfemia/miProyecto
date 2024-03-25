043a355f8bbf11fedb9496e4ee651536584f61f6
kael <kaellopez987@gmail.com>
2024-03-25 12:03:41 -0500

    25 de marzo

--- src/app.js
+++ src/app.js
@@ -1,14 +1,21 @@
 import  express  from "express";
 import morgan from "morgan";
+import cookieParser from "cookie-parser";
+
 import authRoutes from './routes/auth.routes.js'
+import taskRoutes from './routes/tasks.routes.js'
 
 
 const app = express();
 
 app.use(morgan('dev'));
 app.use(express.json())
+app.use(cookieParser())
 
-app.use('/api',authRoutes);
+
+//Se realiza el llamado de las rutas
+app.use('/api', authRoutes);
+app.use('/api', taskRoutes);
 
 
 
--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -65,5 +65,17 @@ export const register = async (req,res) => {
     res.cookie("token", "", {
         expires: new Date(0)
     })
-    return res.sentStatus(200);
+    return res.sendStatus(200);
+ };
+
+ export const profile = async (req, res) =>{
+    const userFound = await User.findById(req.user.id)
+
+    if (!userFound) return res.status(400).json({message: 'Usuario no encontrado'})
+
+    return res.json({
+        id: userFound.id,
+        username: userFound.username,
+        email: userFound.email
+    })
  }
